d0dfd7472e0d9069f5a2f67f6260b72c3edaa7bf
George Chartouny <chartounygeorge@gmail.com>
2023-10-30 10:10:46 +0200

    Exception handling connection

--- NI-BE/NI-BE/DataDb/DBConnection.cs
+++ NI-BE/NI-BE/DataDb/DBConnection.cs
@@ -21,54 +21,59 @@ namespace NI_BE.DataDb
             VerticaConnection _conn = new VerticaConnection(builder.ToString());
             try
             {
-            _conn.Open();
+                _conn.Open();
+                Console.WriteLine("Connection Established.");
+
+                try
+                {
+                    using (_conn)
+                    {
+
+
+                        //Create command
+                        VerticaCommand command = _conn.CreateCommand();
+                        command.CommandText = "select * from fact_TransactionsWeekly;";
+                        command.CommandText += "create table if not exists TESTDOTNET(id INT PRIMARY KEY,Gender VARCHAR(255))SEGMENTED BY HASH(ID) ALL NODES";
+
+
+                        //Associate the command with the connection
+                        command.Connection = _conn;
+
+                        //Create the DataAdapter
+                        VerticaDataAdapter adapter = new VerticaDataAdapter();
+                        adapter.SelectCommand = command;
+
+                        //Fill the table
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+
+                        // Display each row and column value
+                        int i = 1;
+                        foreach (DataRow row in table.Rows)
+                        {
+                            foreach (DataColumn column in table.Columns)
+                            {
+                                Console.WriteLine(row[column] + "\t");
+                            }
+                            Console.WriteLine();
+                            i++;
+                        }
+                        Console.WriteLine(i + "rows returned.");
+                    }
+                    _conn.Close();
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error esablishing connection: " + e.Message);
+                }
 
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine("mafi connection: " + ex.Message);
-            }
-
-            try
-            {
-                using (_conn)
-                {
-                    //Create command
-                    VerticaCommand command = _conn.CreateCommand();
-                    command.CommandText = "select * from fact_TransactionsWeekly;";
-
-                    //Associate the command with the connection
-                    command.Connection = _conn;
-
-                    //Create the DataAdapter
-                    VerticaDataAdapter adapter = new VerticaDataAdapter();
-                    adapter.SelectCommand = command;
-
-                    //Fill the table
-                    DataTable table = new DataTable();
-                    adapter.Fill(table);
-
-                    // Display each row and column value
-                    int i = 1;
-                    foreach (DataRow row in table.Rows)
-                    {
-                        foreach (DataColumn column in table.Columns)
-                        {
-                            Console.WriteLine(row[column] + "\t");
-                        }
-                        Console.WriteLine();
-                        i++;
-                    }
-                    Console.WriteLine(i + "rows returned.");
-                }
-                _conn.Close();
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine("Error esablishing connection: " + e.Message);
+                Console.WriteLine("Failed to connect to the database: " + ex.Message);
             }
 
         }

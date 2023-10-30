c71b9ce53492ecf1496fdf0ba3a22b6824078f68
George Chartouny <chartounygeorge@gmail.com>
2023-10-30 11:33:09 +0200

    env variables for db conn

--- NI-BE/NI-BE/DataDb/DBConnection.cs
+++ NI-BE/NI-BE/DataDb/DBConnection.cs
@@ -12,11 +12,12 @@ namespace NI_BE.DataDb
         public void EstablishConnection()
         {
             VerticaConnectionStringBuilder builder = new VerticaConnectionStringBuilder();
-            builder.Host = "10.10.4.231";
+            //builder.Host = "10.10.4.231";
+            builder.Host = Environment.GetEnvironmentVariable("HOST");
             builder.Port = 5433;
-            builder.Database = "test";
-            builder.User = "bootcamp5";
-            builder.Password = "bootcamp52023";
+            builder.Database = Environment.GetEnvironmentVariable("DATABASE");
+            builder.User = Environment.GetEnvironmentVariable("USERNAME");
+            builder.Password = Environment.GetEnvironmentVariable("PASSWORD");
 
             VerticaConnection _conn = new VerticaConnection(builder.ToString());
             try
@@ -34,7 +35,31 @@ namespace NI_BE.DataDb
                         VerticaCommand command = _conn.CreateCommand();
                         command.CommandText = "select * from fact_TransactionsWeekly;";
                         command.CommandText += "create table if not exists TESTDOTNET(id INT PRIMARY KEY,Gender VARCHAR(255))SEGMENTED BY HASH(ID) ALL NODES";
-
+                       // Command for tables to create if they do not exist in the database
+                        command.CommandText += "CREATE TABLE IF NOT EXISTS TRANS_MW_ERC_PM_TN_RADIO_LINK_POWER (" +
+                            "NETWORK_SID INT NOT NULL," +
+                            "DATETIME_KEY TIMESTAMPTZ NOT NULL," +
+                            "NEID FLOAT NOT NULL," +
+                            "OBJECT TEXT NOT NULL," +
+                            "TIME TIMESTAMPTZ NOT NULL," +
+                            "INTERVAL INT NOT NULL," +
+                            "DIRECTION TEXT NOT NULL," +
+                            "NEALIAS TEXT NOT NULL," +
+                            "NETYPE TEXT NOT NULL," +
+                            "RXLEVELBELOWTS1 FLOAT NOT NULL," +
+                            "RXLEVELBELOWTS2 FLOAT NOT NULL," +
+                            "MINRXLEVEL FLOAT NOT NULL," +
+                            "MAXRXLEVEL FLOAT NOT NULL," +
+                            "TXLEVELABOVETS1 FLOAT NOT NULL," +
+                            "MINTXLEVEL FLOAT NOT NULL," +
+                            "MAXTXLEVEL FLOAT NOT NULL," +
+                            "FAILUREDESCRIPTION TEXT NOT NULL," +
+                            "LINK TEXT NOT NULL," +
+                            "TID TEXT NOT NULL," +
+                            "FARENDTID TEXT NOT NULL," +
+                            "SLOT TEXT NOT NULL," +
+                            "PORT INT NOT NULL," +
+                            ")SEGMENTED BY HASH(NETWORK_SID,DATETIME_KEY) ALL NODES;";
 
                         //Associate the command with the connection
                         command.Connection = _conn;

ce0be54f62908e9d65cc2b0b94d4f29fd8156f70
George Chartouny <chartounygeorge@gmail.com>
2023-11-20 12:05:57 +0200

    allowing cors origin

--- babyNi-GetDataAPI/babyNi-GetDataAPI/Program.cs
+++ babyNi-GetDataAPI/babyNi-GetDataAPI/Program.cs
@@ -2,6 +2,17 @@ using babyNi_GetDataAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                     .AllowAnyMethod();
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -18,6 +29,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAllOrigins");
+
 
 app.UseHttpsRedirection();
 

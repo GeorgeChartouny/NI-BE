bbf5b77f91b2749dd04c0afc9764697275780b95
George Chartouny <chartounygeorge@gmail.com>
2023-11-27 10:35:58 +0200

    loaderAPI app

---
+++ babyNI-LoaderAPI/babyNI-LoaderAPI/Models/FileUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace babyNI_LoaderAPI.Models
+{
+    public class FileUpload
+    {
+        public IFormFile File { get; set; }
+
+    }
+}
---
+++ babyNI-LoaderAPI/babyNI-LoaderAPI/Program.cs
@@ -0,0 +1,38 @@
+using babyNI_LoaderAPI.Services;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddTransient<LoaderService>();
+
+
+// Serilog Configuration
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+builder.Host.UseSerilog(); //log on each request 
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
